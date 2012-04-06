69258288369a5b06feb7d65344a0ae13a08c5cb6
jake-wishcompany <jake@wishcompany.net>
2012-04-06 15:46:42 +0900

    first commit

---
+++ app/controllers/demo_controller.rb
@@ -0,0 +1,19 @@
+class DemoController < ApplicationController
+  def index
+  	#render(:action => 'hello')
+  	#render(:template => 'demo/hello')
+  	#render('demo/hello')
+  	#render('hello')
+  	redirect_to(:action => 'other_hello')
+  	#redirect_to(:controller => 'demo', :action=>'other_hello')
+  end
+  
+  def hello
+  	#redirect_to('http://www.lynda.com')
+  	@array = [1,2,3,4,5]
+  end
+  
+  def other_hello
+  	render(:text => 'Hello Everyone!')
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleCms::Application.config.secret_token = '41670cbadb46c9ed795b554f0fda7ff9f54226ff7ce50387aace0437340dd01f8acca03dda59efdc8366e799220b417f6a00aee09a25cf998620eb8e61781318'
